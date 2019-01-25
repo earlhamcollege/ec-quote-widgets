437e75d1f96579c4b5f33046c42f3f545b779a12
probably-not-porter <pelibby16@earlham.edu>
2019-01-25 14:09:44 -0500

    add loading and loaded styles to hide loading struct

--- index.js
+++ index.js
@@ -177,7 +177,6 @@ module.exports = {
   },
   construct: function (self, options) {
     var superPushAssets = self.pushAssets;
-
     self.pushAssets = function() {
       superPushAssets();
       self.pushAsset('stylesheet','variables');
--- public/js/always.js
+++ public/js/always.js
@@ -2,12 +2,17 @@ apos.define('ec-quote-widgets', {
   extend: 'apostrophe-widgets',
   construct: function(self, options) {
       self.play = function($widget, data, options) {
-        console.log(data);
+        //post load
+        console.log(data.fontSize);
+        $widget.find('[data-loading]').fadeIn().attr('data-loading', 'data-loaded');
+        $widget.find('[data-ec-quote] h1').css('font-size', data.fontSize+'vw');
+
+        //ctrl
         var instance = data._id;
         var inst_speed = data.speed * 1000 || 5000;
         var inst_autoplay = data.autoplay_toggle;
         var inst_dots = data.nav_toggle;
-        
+
       $widget.find("[data-ec-quote-"+data._id+"] .slider").slick({
         infinite: true,
         slidesToShow: 1,
